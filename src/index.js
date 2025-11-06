17f4061080abadb4cac71649b0055703c2b366c8
avilca <avilca85@gmail.com>
2025-11-06 18:23:57 -0500

    Mi primera clase

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Greeting() {
+  function add(x, y) {
+    return x + y;
+  }
+
+  return (
+    <div>
+      <h1>{add(10, 30)}</h1>
+    </div>
+  );
+}
+
+root.render(
+  <>
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+  </>
+);
