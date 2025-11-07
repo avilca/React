66ca3a0419daa172773f4c98191ea8ef60d6e4c6
avilca <avilca85@gmail.com>
2025-11-07 12:05:20 -0500

    Modulos

---
+++ src/Greeting.js
@@ -0,0 +1,8 @@
+/* Exportar de forma individual */
+export function Greeting() {
+  return <h1>Hola, Bienvenido a React</h1>
+}
+
+export function UserCard() {
+    return <h1>Tarjeta de Usuario</h1>
+}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Greeting, UserCard } from "./Greeting"; /* Importar de forma individual */
+import Product, {Navbar} from "./Product"; /* Importar de forma por defecto */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Greeting() {
-  function add(x, y) {
-    return x + y;
-  }
-
-  return (
-    <div>
-      <h1>{add(10, 30)}</h1>
-    </div>
-  );
-}
 
 root.render(
   <>
     <Greeting />
-    <Greeting />
-    <Greeting />
-    <Greeting />
+    <UserCard />
+    <Product />
   </>
 );
