7eac868eae9f2e5d59fe30f1c11deba20a0a532b
avilca <avilca85@gmail.com>
2025-11-07 12:56:22 -0500

    props

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,8 +1,22 @@
 /* Exportar de forma individual */
-export function Greeting() {
-  return <h1>Hola, Bienvenido a React</h1>
+export function Greeting({ title, name = "Footgolito" }) {
+  return (
+    <h1>
+      {title}, dice {name}
+    </h1>
+  );
 }
 
-export function UserCard() {
-    return <h1>Tarjeta de Usuario</h1>
+export function UserCard({ name, amount, married, address, greet }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>💲{amount}</p>
+      <p>{married ? "married" : "single"}</p>
+      <ul>
+        <li>City: {address.city}</li>
+        <li>Address: {address.street}</li>
+      </ul>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Greeting, UserCard } from "./Greeting"; /* Importar de forma individual */
-import Product, {Navbar} from "./Product"; /* Importar de forma por defecto */
+import {
+  Greeting,
+  UserCard,
+} from "./Greeting"; /* Importar de forma individual */
+import Product, { Navbar } from "./Product"; /* Importar de forma por defecto */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
 root.render(
   <>
-    <Greeting />
-    <UserCard />
-    <Product />
+    <UserCard
+      name="Footgolito golito"
+      amount={3000}
+      married={true}
+      points={[99, 33.3, 22.2]}
+      address={{ street: "123 Main Street", city: "New York" }}
+      greet={function () { alert('Hello') }}
+    />
+
+    <UserCard 
+      name= "Andres McMillan"
+      amount={1000}
+      married={false}
+      points={[100, 20]}
+      address={{ 
+        street: "Avenida 3 de octubre 782", 
+        city: "Comas" 
+      }}
+      
+    />
+
   </>
 );
