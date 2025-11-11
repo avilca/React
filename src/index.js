9a928af5df36fef6fee09760b11cc0d973f215ca
avilca <avilca85@gmail.com>
2025-11-11 18:08:35 -0500

    Estilos

--- src/index.js
+++ src/index.js
@@ -7,33 +7,14 @@ import {
 import Product, { Navbar } from "./Product"; /* Importar de forma por defecto */
 
 import {Button} from "./Button";
+import {Taskcard} from "./Task";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <Button text='Click me' />
-    <Button text='Pay' />
-    <Button text='' name='Jhony ' />
-
-      {/* UserCard
-      name="Footgolito golito"
-      amount={3000}
-      married={true}
-      points={[99, 33.3, 22.2]}
-      address={{ street: "123 Main Street", city: "New York" }}
-      greet={function () { alert('Hello') }}
-    />
-
-    <UserCard 
-      name= "Andres McMillan"
-      amount={1000}
-      married={false}
-      points={[100, 20]}
-      address={{ 
-        street: "Avenida 3 de octubre 782", 
-        city: "Comas" 
-      }} */}
+    <Taskcard ready={true} />
       
     
 
