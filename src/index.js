66d7356913d8cd737b67215ae50660fe125e4fbf
avilca <avilca85@gmail.com>
2025-11-12 19:36:36 -0500

    Manejadores de eventos

--- src/index.js
+++ src/index.js
@@ -8,15 +8,27 @@ import Product, { Navbar } from "./Product"; /* Importar de forma por defecto */
 
 import {Button} from "./Button";
 import {Taskcard} from "./Task";
-
+import { Saludar } from "./Saludar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+
 
 root.render(
   <>
     <Taskcard ready={true} />
-      
+    <Saludar />
+    <Button text="Saludar"/>  
     
+    <form onSubmit={(e) => {
+      e.preventDefault()
+      alert('Enviado')
+    }}>
+
+      <h1>Registro de usuario</h1>
+      <button>Send</button>
+    </form>
 
   </>
 );
