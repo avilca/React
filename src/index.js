9980acfe4b4763ed5712c577b1bf69664272fd62
avilca <avilca85@gmail.com>
2025-11-12 19:52:58 -0500

    fetch API

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import Product, { Navbar } from "./Product"; /* Importar de forma por defecto */
 import {Button} from "./Button";
 import {Taskcard} from "./Task";
 import { Saludar } from "./Saludar";
+import { Posts } from "./Posts";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -17,18 +19,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <Taskcard ready={true} />
-    <Saludar />
-    <Button text="Saludar"/>  
-    
-    <form onSubmit={(e) => {
-      e.preventDefault()
-      alert('Enviado')
-    }}>
-
-      <h1>Registro de usuario</h1>
-      <button>Send</button>
-    </form>
+    <Posts/>
 
   </>
 );
