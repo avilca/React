aeb251834c3a4b1dbe350e99d88f2fdef6f0e454
avilca <avilca85@gmail.com>
2025-11-12 20:27:47 -0500

    Arreglos en React

--- src/index.js
+++ src/index.js
@@ -6,20 +6,35 @@ import {
 } from "./Greeting"; /* Importar de forma individual */
 import Product, { Navbar } from "./Product"; /* Importar de forma por defecto */
 
-import {Button} from "./Button";
-import {Taskcard} from "./Task";
+import { Button } from "./Button";
+import { Taskcard } from "./Task";
 import { Saludar } from "./Saludar";
 import { Posts } from "./Posts";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-
+const users = [
+  {
+    id: 1,
+    name: "Andres",
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 2,
+    name: "joe",
+    image: "https://robohash.org/user2",
+  },
+];
 
 root.render(
   <>
-    <Posts/>
-
+    {users.map((user, i) => {
+      return (
+        <div key={i}>
+          <h1>{user.name}</h1>
+          <img src={user.image} />
+        </div>
+      );
+    })}
   </>
 );
