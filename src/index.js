45899a8090e7017e1256f29616cc6645f2f25b91
avilca <avilca85@gmail.com>
2025-11-13 11:39:19 -0500

    React Hooks - useState

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from 'react';
 import ReactDOM from "react-dom/client";
 import {
   Greeting,
@@ -13,28 +13,39 @@ import { Posts } from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const users = [
-  {
-    id: 1,
-    name: "Andres",
-    image: "https://robohash.org/user1",
-  },
-  {
-    id: 2,
-    name: "joe",
-    image: "https://robohash.org/user2",
-  },
-];
+function Counter() {
+
+  const [ counter, setCounter ] = useState(0)
+
+
+  return (
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => {
+        setCounter( counter + 1 );
+      }}>
+        Sumar
+      </button>
+
+      <button onClick={() => {
+        setCounter(counter - 1)
+      }}>
+        Restar
+      </button>
+
+      <button onClick={() => {
+        setCounter(1000)
+      }}>
+        Reiniciar
+      </button>
+
+
+    </div>
+  )
+}
 
 root.render(
   <>
-    {users.map((user, i) => {
-      return (
-        <div key={i}>
-          <h1>{user.name}</h1>
-          <img src={user.image} />
-        </div>
-      );
-    })}
+    <Counter/>
   </>
 );
